9cad737a4d9a56057441c10121da9d52ca3cbfdd
natalielyn <nlmaxwell@gmail.com>
2020-02-09 13:23:42 -0800

    created index.js

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,6 +1,7 @@
+
 'use strict';
 
-process.env.SECRET = 'secrets';
+process.env.SECRET='secrets';
 
 const jwt = require('jsonwebtoken');
 
@@ -10,24 +11,26 @@ const supergoose = require('./supergoose.js');
 const mockRequest = supergoose.server(server);
 
 let users = {
-  admin: { username: 'admin', password: 'password', role: 'admin' },
-  editor: { username: 'editor', password: 'password', role: 'editor' },
-  user: { username: 'user', password: 'password', role: 'user' },
+  admin: {username: 'admin', password: 'password', role: 'admin'},
+  editor: {username: 'editor', password: 'password', role: 'editor'},
+  user: {username: 'user', password: 'password', role: 'user'},
 };
 
 beforeAll(supergoose.startDB);
 afterAll(supergoose.stopDB);
 
 describe('Auth Router', () => {
-  Object.keys(users).forEach(userType => {
+
+  Object.keys(users).forEach( userType => {
+
     describe(`${userType} users`, () => {
+
       let id;
       let token;
       let resultsToken;
 
       it('Can create user', () => {
-        return mockRequest
-          .post('/v1/signup')
+        return mockRequest.post('/v1/signup')
           .send(users[userType])
           .then(results => {
             resultsToken = results.text;
@@ -38,8 +41,7 @@ describe('Auth Router', () => {
       });
 
       it('Can authenticate user on signin', () => {
-        return mockRequest
-          .post('/v1/signin')
+        return mockRequest.post('/v1/signin')
           .auth(users[userType].username, users[userType].password)
           .then(results => {
             var token = jwt.verify(results.text, process.env.SECRET);
@@ -48,8 +50,7 @@ describe('Auth Router', () => {
       });
 
       it('Returns single user', () => {
-        return mockRequest
-          .get('/v1/user')
+        return mockRequest.get('/v1/user')
           .set('Authorization', `Bearer ${resultsToken}`)
           .then(results => {
             expect(results.status).toEqual(200);
@@ -59,17 +60,19 @@ describe('Auth Router', () => {
   });
 
   it('/users returns all users', () => {
-    return mockRequest.get('/v1/users').then(data => {
-      expect(data.body.count).toEqual(3);
-    });
+    return mockRequest.get('/v1/users')
+      .then(data => {
+        expect(data.body.count).toEqual(3);
+      });
   });
 
   it('Returns invalid login when wrong header', () => {
-    return mockRequest
-      .post('/v1/signin')
-      .auth({ name: 5, password: 6 })
+    return mockRequest.post('/v1/signin')
+      .auth({name: 5, password: 6})
       .then(results => {
         expect(results.status).toEqual(500);
       });
   });
+
+
 });
